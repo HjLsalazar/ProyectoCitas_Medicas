2784f96063059539ab9c0ef9a1b0713b8d33db5f
HjLsalazar <163606026+HjLsalazar@users.noreply.github.com>
2025-08-24 10:51:20 -0600

    Añadir clase Cita en el espacio de nombres Models
    Se ha creado un nuevo espacio de nombres `Models` que incluye la clase `Cita`. Esta clase define propiedades como `CitaId`, `PacienteId`, `DoctorId`, `FechaHora`, `DuracionMinutos`, `Motivo` y `Estado`. Se han implementado dos constructores: uno por defecto y otro que permite inicializar todas las propiedades al instanciar la clase.

---
+++ LoginRoles/Models/Citas.vb
@@ -0,0 +1,26 @@
+﻿
+Namespace Models
+    Public Class Cita
+        Public Property CitaId As Integer
+        Public Property PacienteId As Integer
+        Public Property DoctorId As Integer
+        Public Property FechaHora As DateTime
+        Public Property DuracionMinutos As Integer
+        Public Property Motivo As String
+        Public Property Estado As String
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(citaId As Integer, pacienteId As Integer, doctorId As Integer,
+                       fechaHora As DateTime, duracion As Integer, motivo As String, estado As String)
+            Me.CitaId = citaId
+            Me.PacienteId = pacienteId
+            Me.DoctorId = doctorId
+            Me.FechaHora = fechaHora
+            Me.DuracionMinutos = duracion
+            Me.Motivo = motivo
+            Me.Estado = estado
+        End Sub
+    End Class
+End Namespace
