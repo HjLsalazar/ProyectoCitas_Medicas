2a76968ff42c1eb0eaf5ddd7d5bc82f8e6ff5214
HjLsalazar <163606026+HjLsalazar@users.noreply.github.com>
2025-08-24 10:49:34 -0600

    Añadir clase Doctor en el espacio de nombres Models
    Se ha creado la clase `Doctor` que hereda de `Usuario`. Esta clase incluye propiedades para `DoctorId`, `Nombre`, `Especialidad`, `Correo` y `Telefono`. Se han implementado dos constructores: uno por defecto y otro que permite inicializar todas las propiedades al instanciar la clase.

---
+++ LoginRoles/Models/Doctor.vb
@@ -0,0 +1,26 @@
+﻿
+Namespace Models
+    Public Class Doctor
+        Inherits Usuario
+
+        Public Property DoctorId As Integer
+        Public Property Nombre As String
+        Public Property Especialidad As String
+        Public Property Correo As String
+        Public Property Telefono As String
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+        Public Sub New(doctorId As Integer, nombre As String, especialidad As String,
+                       correo As String, telefono As String)
+            MyBase.New()
+            Me.DoctorId = doctorId
+            Me.Nombre = nombre
+            Me.Especialidad = especialidad
+            Me.Correo = correo
+            Me.Telefono = telefono
+        End Sub
+    End Class
+End Namespace
